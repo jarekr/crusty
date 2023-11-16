e1e60f5efa15ec52095aa94d7b5418ea5fb2f62c
Jarek Rudzinski <jarek@chunski.com>
2023-11-16 19:31:10 +0000

    src/db.rs

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,5 @@
 use const_format::concatcp;
-use rusqlite::{named_params, Connection, OpenFlags};
+use rusqlite::{named_params, Connection, OpenFlags, Error};
 use std::path::Path;
 
 const GAMES_TABLE: &str = "games";
@@ -12,15 +12,35 @@ const GAMES_DDSQL: &str = concatcp!(
             notes    TEXT
         )"
 );
-const GET_BY_ID_GAMES_QSQL: &str = concatcp!(
+const GET_BY_ID_GAMES_SQL: &str = concatcp!(
     "SELECT * FROM ",
     GAMES_TABLE,
     " where id = :id order by id asc"
 );
-const INSERT_INTO_GAMES_QSQL: &str = concatcp!(
+const INSERT_INTO_GAMES_SQL: &str = concatcp!(
     "INSERT INTO ",
     GAMES_TABLE,
-    " (id, pgn, notes) VALUES (:id, :pgn, :notes)"
+    " (pgn, notes) VALUES (:pgn, :notes)"
+);
+
+const POSITIONS_TABLE: &str = "positions";
+
+const POSITIONS_DDSQL: &str = concatcp!(
+    "CREATE TABLE IF NOT EXISTS ",
+    POSITIONS_TABLE,
+    " (
+            id       INTEGER PRIMARY KEY,
+            r12      INTEGER,
+            r34      INTEGER,
+            r56      INTEGER,
+            r78      INTEGER
+      )"
+);
+
+const INSERT_INTO_POSITIONS_SQL: &str = concatcp!(
+    "INSERT INTO ",
+    POSITIONS_TABLE,
+    " (r12, r34, r56, r78) VALUES (:r12, :r34, :r56, :r78)"
 );
 
 pub struct Db {
@@ -65,7 +85,7 @@ impl Game {
     pub fn query_by_id(db: Db, id: u32) -> Option<Game> {
         let mut stmt = db
             .conn
-            .prepare(GET_BY_ID_GAMES_QSQL)
+            .prepare(GET_BY_ID_GAMES_SQL)
             .expect("prepare failed");
         let mut row_iter = stmt.query(named_params! {":id": id.to_string()}).unwrap();
 
@@ -79,3 +99,22 @@ impl Game {
         }
     }
 }
+
+pub struct Position {
+    pub id: u32,
+    pub r12: u64,
+    pub r34: u64,
+    pub r56: u64,
+    pub r78: u64,
+}
+
+impl Position {
+    pub fn insert(db: Db, r12: u64, r34: u64, r56: u64, r78: u64) -> Result<usize, Error> {
+        let mut stmt = db
+            .conn
+            .prepare(INSERT_INTO_POSITIONS_SQL)
+            .expect("prepare failed");
+        stmt.execute(named_params! {":r12": r12, ":r34": r34, ":r56": r56, ":r78": r78 })
+    }
+}
+
