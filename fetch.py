115c6af4f043f856206c39d7c5cada823ca078a9
Jarek Rudzinski <jarek@chunski.com>
2024-03-23 16:25:53 -0700

    fetch.py

--- fetch.py
+++ fetch.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
 import requests
+import sys
 
-url_template = "https://api.chess.com/pub/player/jrudzinski/games/{year}/{month}/pgn"
+url_template = "https://api.chess.com/pub/player/{player}/games/{year}/{month}/pgn"
 
-result = requests.get(url_template.format(year=2020, month=10))
+if len(sys.argv) < 4:
+    print("Usage: <user> <yyyy> <mm>")
+    sys.exit(2)
+
+player = sys.argv[1]
+year = sys.argv[2]
+month = sys.argv[3]
+
+myurl = url_template.format(year=year, month=month, player=player)
+result = requests.get(myurl)
 import pdb; pdb.set_trace()
-print("all done")
+print("fetched "+myurl)
+print(result)
